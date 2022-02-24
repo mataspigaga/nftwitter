dc5672597be692c9fe7dd11f9fd025a72c513e03
Matas Pigaga <tech@matas.io>
2022-02-24 12:28:09 -0500

    added basic navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import classes from "./styles/App.module.css";
+import { useState } from "react";
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState("false");
+
+  const logInHandler = () => {
+    setLoggedIn((prevState) => !prevState);
+  };
+
   return (
-    <div>
-      <header>
-        <h1>NFTwitter</h1>
-        <nav></nav>
+    <div className={classes.home}>
+      <header className={classes.header}>
+        <div className={classes.container}>
+          <h1 className={classes.logo}>NFTwitter</h1>
+          <nav className={classes.nav}>
+            {loggedIn && <a href="/">Profile</a>}
+            <button onClick={logInHandler}>
+              {loggedIn ? "Disconnect" : "Connect Wallet"}
+            </button>
+          </nav>
+        </div>
       </header>
-      <main>
-        <section>
-          <div></div>
+      <main className={classes.main}>
+        <section className={classes.section}>
+          <div className={classes.container}></div>
         </section>
       </main>
     </div>
