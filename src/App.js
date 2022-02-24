edafcf7f1ce4a86c0dfb314908d1871c77e02642
Matas Pigaga <tech@matas.io>
2022-02-24 11:22:14 -0500

    started base code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from "./styles/App.module.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>NFTwitter</h1>
+        <nav></nav>
       </header>
+      <main>
+        <section>
+          <div></div>
+        </section>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
